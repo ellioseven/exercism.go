00cba8534f42727a7254ca7a512f69c6458ef919
Elliot Mitchum <elliot.mitchum@ellioseven.com.au>
2022-05-07 14:46:14 +1000

    Update

---
+++ difference-of-squares/difference_of_squares.go
@@ -0,0 +1,25 @@
+package diffsquares
+
+func SquareOfSum(n int) int {
+	sum := 0
+
+	for i := 0; i <= n; i++ {
+		sum += i
+	}
+
+	return sum * sum
+}
+
+func SumOfSquares(n int) int {
+	sum := 0
+
+	for i := 0; i <= n; i++ {
+		sum += i * i
+	}
+
+	return sum
+}
+
+func Difference(n int) int {
+	return SquareOfSum(n) - SumOfSquares(n)
+}
